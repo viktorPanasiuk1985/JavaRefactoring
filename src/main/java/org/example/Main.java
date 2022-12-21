b0996bfce1cbfb00902924f2d0abe4e0986f6516
viktor.panasiuk <vpanasiuk@regtek.solutions>
2022-12-22 01:09:06 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        PetrolStation station = new PetrolStation(100, PetrolStation.fillCarsQueue(3));
+        System.out.println("Количество топлива на станции - " + station.getAmount());
+        for (int i = 0; i < 7; i++) {
+            station.doRefuel(15);
+        }
+    }
+}
---
+++ src/main/java/org/example/PetrolStation.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Random;
+
+public class PetrolStation {
+    private double amount;
+    private Queue<FuelDispenser> carsQueue;
+
+    public PetrolStation(double amount, Queue<FuelDispenser> carsQueue) {
+        this.amount = amount;
+        this.carsQueue = carsQueue;
+    }
+
+    public static Queue<FuelDispenser> fillCarsQueue(int count) {
+        Queue<FuelDispenser> carsQueue = new ArrayDeque<>();
+        for (int i = 0; i < count; i++) {
+            carsQueue.add(new FuelDispenser());
+        }
+        return carsQueue;
+    }
+
+    void doRefuel(double subtrack) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (carsQueue) {
+                        while (true) {
+                            if (!carsQueue.isEmpty()) {
+                                if (amount < subtrack) {
+                                    System.out.println(Thread.currentThread().getName() + " - Извините топлива вам не хватит. Вы запросили:" + subtrack + ". а осталось: " + amount);
+                                    carsQueue.notify();
+                                    return;
+                                }
+                                FuelDispenser fuelDispenser = carsQueue.remove();
+                                int refuelingTime = (new Random().nextInt(10000 - 3000) + 3000);
+                                System.out.println(Thread.currentThread().getName() + " oбслуживаеться " + refuelingTime + " мс на колонке -> " + fuelDispenser);
+                                carsQueue.wait(refuelingTime);
+                                amount = amount - subtrack;
+                                System.out.println("Осталось " + amount + " топлива, " + Thread.currentThread().getName() + "  освобождает колонку -> " + fuelDispenser);
+                                carsQueue.add(fuelDispenser);
+                                carsQueue.notify();
+                                break;
+                            } else {
+                                System.out.println(Thread.currentThread().getName() + " ожидает свободную колонку");
+                                carsQueue.wait();
+                            }
+                        }
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
